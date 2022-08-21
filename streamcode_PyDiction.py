543d701ee7f20650dd95b766e5e1b5bcc5b8806d
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-08-21 19:08:27 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -6,4 +6,4 @@ st.title("Hello world!")
 uploaded_file = st.file_uploader("cliquer sur 'Browse' pour charger vos données")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  st.write(df)
