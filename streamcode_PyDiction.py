37205dadd5bb83cb6180501cc4909c41b3516fb3
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-08-21 19:49:26 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -2,11 +2,18 @@ import pandas as pd
 import streamlit as st
 
 st.title("PyDiction")
-st.title("Ce projet est réalisé dans le cadre d'une formation professionnelle en Data Science.")
-st.title("Ce projet est réalisé dans le cadre d'une formation professionnelle en Data Science.")
+st.mark("Ce projet est réalisé dans le cadre d'une formation professionnelle en Data Science.")
+st.mark("C'est un travail autour de la météorologie et du Machine Learning (ML).")
+st.mark("Procédure : il vous faudra charger des données de météorologie (températures etc) les plus conséquentes possibles ")
+st.mark("au FORMAT CSV. Ensuite, vous pourrez suivre les étapes et répondre aux questions jusqu'à la prédiction de la présence de pluie ")
+st.mark("à J+1, sur un point quelconque du territoire australien.")
+st.mark("Le score de prédiction sera affiché en fin. Les données rentrées devront être du type ")
+
+st.title("Première partie")
 
 
 uploaded_file = st.file_uploader("cliquer sur 'Browse' pour charger vos données")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
+
