750cb73546cbcf53181c58e9d4e6e1cb94d49d8d
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-08-21 20:02:33 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -20,4 +20,5 @@ uploaded_file = st.file_uploader("cliquer sur 'Browse' pour charger vos données
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
+  st.write(df.info())
 
