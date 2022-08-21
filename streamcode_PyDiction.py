bc6f519c6e955d0ce7d56cb343709345e2012507
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-08-21 19:10:42 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Hello world!")
+st.title("PyDiction")
+st.subtitle("PyDiction")
+
 
 uploaded_file = st.file_uploader("cliquer sur 'Browse' pour charger vos données")
 if uploaded_file is not None:
