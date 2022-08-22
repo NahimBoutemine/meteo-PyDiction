88f0bf94b3c8eeb071f204c88e2caf4b721f4163
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-08-22 22:42:50 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -29,15 +29,12 @@ from sklearn import metrics
 st.title("PyDiction")
 
 st.markdown("Ce projet est réalisé dans le cadre d'une formation professionnelle en Data Science.")
-st.markdown("C'est un travail autour de la météorologie et du Machine Learning (ML). Il permet de prédire si la pluie est présente sur un point quelconque australien à partir de données météorologiques telles que décrites plus bas. ")
-st.markdown("Procédure : il vous faudra charger des données de météorologie (températures etc) les plus conséquentes possibles ")
-st.markdown("au FORMAT CSV. Ensuite, vous pourrez suivre les étapes et répondre aux questions jusqu'à la création du modèle et la prédiction de la présence ou non de pluie à J+1, sur un point quelconque du territoire australien. ")
-st.markdown("Le score de prédiction sera affiché en fin. Les données rentrées devront être du même type que ce sur ce lien https://www.kaggle.com/jsphyg/weather-dataset-rattle-package, car le modèle est construit à partir de ces dernières")
-st.markdown("C'est-à-dire sur 49 stations, sur plusieurs années, et comprenant les données complètes chaque jour jusqu'à la veille de la journée à prédire : ensoleillement, humidité, vitesse et sens du vent, quantité de nuages, températures minimales et maximales etc")
-st.markdown("Les formats des données doivent être identiques notamment pour la date et la variable cible, RainTomorrow. Toute donnée supplémentaire peut être intéressante mais peut demander un traitement de conversion.")
-st.markdown("La pluie est considérée comme présente si elle est strictement supérieure à 1mm. ")
-st.markdown("En plus du guidage sur la préparation de vos données et la création du modèle, vous profiterez d'affichages de'indicateurs et de graphiques décrivant vos données ")
-st.markdown("Vous pouvez donc commencer par charger vos données et observer leur première description! ")
+st.markdown("C'est un travail autour de la météorologie et du Machine Learning (ML). Cette application streamlit sert à montrer les résultats de notre construction de modèle de prédiction de la variable 'RainTomorrow' à partir du dataset déposé ici https://www.kaggle.com/jsphyg/weather-dataset-rattle-package")
+st.markdown("Le modèle nous permet de prédire effectivement la présence de pluie au lendemain avec une précision de plus de 85%, ce qui est acceptable en météorologie. Procédure : il vous faudra charger des données du site kaggle. ")
+st.markdown("au FORMAT CSV. Ensuite, vous pourrez voir les étapes de construction du modèle et constater ses performances. ")
+st.markdown("Le score de prédiction sera affiché en fin. ")
+st.markdown("Les données sont présentes sur 49 stations, sur plusieurs années, et comprennent les informations de : ensoleillement, humidité, vitesse et sens du vent, quantité de nuages, températures minimales et maximales etc")
+st.markdown("La pluie est considérée comme présente au jour J si elle est strictement supérieure à 1mm. ")
 
 st.header("Première partie")
 
@@ -46,7 +43,7 @@ uploaded_file = st.file_uploader("cliquer sur 'Browse' pour charger vos données
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   
-  st.markdown("Affichons le nombre de lignes, vous devez en avoir au moins 56 000 après suppression des manquantes :")
+  st.markdown("Affichons le nombre de lignes:")
   st.write(len(df))
 
   st.write(df)
@@ -54,7 +51,7 @@ if uploaded_file is not None:
   st.markdown("Affichons le pourcentage de ces fameuses valeurs manquantes, et ce pour chacune des variables :")
   percent_missing_df = df.isnull().sum() * 100 / len(df)
   st.write(percent_missing_df)
-  st.markdown("Ainsi, les valeurs manquantes sont enlevés automatiquement car, même si en général elles sont remplacées par une valeur (imputation statistique), selon notre expérience dans ce cas de prédiction cela ne fait que rajouter du temps de calcul")
+  st.markdown("Ainsi, les valeurs manquantes sont enlevées car, même si en général elles sont remplacées par une valeur (imputation statistique), selon notre expérience dans ce cas de prédiction cela ne fait que rajouter du temps de calcul")
   df = df.dropna()
   df = df.drop_duplicates()
   st.markdown("Vérifiez par vous-même à présent le nombre de données manquantes : on peut voir à présent qu'il n'y en a plus!")
@@ -63,7 +60,7 @@ if uploaded_file is not None:
   st.markdown("Affichons le pourcentage de valeurs non en doublon pour vérifier qu'elles ont été supprimées:")
   percentage_dupli = df.duplicated(keep=False).value_counts(normalize=True) * 100
   st.write(percentage_dupli)
-  st.markdown("Affichons de nouveau le nombre de lignes, pour rappel vous devez en avoir au moins 56 000 à ce stade afin (selon notre expérience) d'avoir un score de prédiction suffisant, sinon vous devez compléter votre dataset :")
+  st.markdown("Affichons de nouveau le nombre de lignes, ce nombre même réduit par rapport au départ (plus de 50% de suppression) nous a permis d'avoir un score de prédiction suffisant :")
   st.write(len(df))
   st.markdown("A présent, les données sont automatiquement encodées, date est transformée en Année, Mois et Jours")
   
@@ -72,8 +69,7 @@ if uploaded_file is not None:
   #encodage des données
   
   ##Traitement de la variable 'date' :
-  #Toutes les variables doivent être encodées et comme la pluie est un phénomène qui semble dépendant des saisons,
-  # il serait interessant de décomposer Date. Pour cela, nous utilisons la méthode 
+
   #dt.datetime pour extraire année, mois, et jour
   df['year'] = pd.to_datetime(df['Date']).dt.year
   df['month'] = pd.to_datetime(df['Date']).dt.month
@@ -96,7 +92,7 @@ if uploaded_file is not None:
   #encodage :     
   for var in df.select_dtypes(include='object').columns:
     df[var] = le.fit_transform(df[var])
-  st.markdown("Voyez par vous-même les créations de variables et l'encodage :")
+  st.markdown("Vérifions les encodages : c'est vérifié. ")
   st.write(df)
 
   #heatmap
@@ -106,15 +102,9 @@ if uploaded_file is not None:
   sns.heatmap(df.corr(), ax=ax)
   st.write(fig)
   st.markdown("Suppression des variables explicatives corréllées à moins de 5% à la cible selon le test de Pearson qui sont 'WindDir3pm','Temp9am','WindDir9am'") 
-  
-  st.markdown('Par hypothèse issue de notre expérience, sur ce type de jeu de données, la direction des vents ainsi que Temp9am sont trop peut corréllées, elles sont donc supprimées automatiquement ')
   df.drop(['WindDir3pm','Temp9am','WindDir9am'], axis = 1) 
   
-
-  ##Découpage des données en jeu d'entrainement et jeu test pour
-  #permettre d'évaluer la performance globale des modèles sur un jeu nouveau,
-  #à hauteur de 20 % des données en test, avec en paramètre randomstate=42 rempli pour la reproductibilité 
-  #des résultats :
+  st.markdown('le jeu de données est ensuite découpé en jeu de test et d entrainement à hauteur de 20% et 80% respectivvement. Puis un rééchantillonnage SMOTE est appliqué puisque nous avons de meilleures performances avec. Cependant il est à noter que les méthodes de normalisation ou de réduction de dimensions n ont pas amené d améloration des résultats, nous ne les avons donc pas conservées. ")
   #(méthode courante d'évaluation):
   y = df['RainTomorrow_encode']
   x = df.drop('RainTomorrow_encode', axis = 1)
@@ -127,13 +117,14 @@ if uploaded_file is not None:
   x_sm_train, x_sm_test, y_sm_train, y_sm_test = train_test_split(x_sm, y_sm, test_size=0.20, random_state=42)
 
   #itération du modèle : KNN, avec les données réduites.
-  st.markdown('Le modèle optimal est selon notre expérience un KNN que nous avons optimisé. Regardons les résultats de prédiction :  ')
+  st.markdown('Le modèle optimal est selon notre expérience un KNN que nous avons optimisé par gridsearch. Evaluons à présent notre modèle :  ')
   model = KNeighborsClassifier(metric='manhattan', n_neighbors=26, weights='distance') #mettre ici le meilleur nbr_voisins trouvé plus haut
   model.fit(x_sm_train,y_sm_train)
 
   ##Précision et f1-score :
   y_pred_train_KNNsm = model.predict(x_sm_train)
   y_pred_test_KNNsm = model.predict(x_sm_test)
+  st.markdown('Les scores d accuracy sur les jeux d entrainement et de test sont :  ')
 
   #accuracy : 
   acc_train_KNNsm  = accuracy_score(y_sm_train, y_pred_train_KNNsm)
@@ -149,11 +140,11 @@ if uploaded_file is not None:
   st.write(pd.crosstab(y_sm_test, y_pred_test_KNNsm, rownames=['Classe réelle'], colnames=['Classe prédite']))
 
   #résultats :
-  st.markdown("hausse à la fois de l'accuracy et du f1 score (0.87 et 0.87 contre 0.85 et 0.74 respectivement pour le KNN sans oversampling")
+  st.markdown("Les prédictions sont plutôt bonnes !")
   st.markdown("meilleur classement des positifs !")
 
   #AUC et ROC Curve:
-  st.markdown('Imprimons à présent la courbe ROC de ce modèle, sont interprétation est donnée ci-dessous. ')
+  st.markdown('Imprimons à présent la courbe ROC de ce modèle : ')
   false_positive_rate, true_positive_rate, thresholds = roc_curve(y_sm_test, model.predict(x_sm_test), pos_label = 1)
   roc_auc_score_KNNsm = roc_auc_score(y_sm_test, model.predict(x_sm_test))
   #la courbe ROC
@@ -166,9 +157,11 @@ if uploaded_file is not None:
   plt.xlabel('False Positive Rate');
   st.pyplot(fig);
   st.write('Le score AUC est de', roc_auc_score_KNNsm, 'interprétation : plus il est proche de 1 plus le modèle est précis, plus il est proche 0.5 moins le modèle est précis.');
- 
-  st.markdown("Le classement des vrais positifs est normalement moins bon que le classement des vrais négatifs")
+  st.markdown('Le score AUC ici est donc acceptable. ')
+  st.markdown("Le classement des vrais positifs est cependant moins bon que le classement des vrais négatifs")
 
   # MAE :
   MAE_KNNsm = mae(y_sm_test, y_pred_test_KNNsm)
-  st.write("La 'Mean Absolute Error' ou 'MAE' est de : " + str(MAE_KNNsm), ', plus elle est basse plus le modèle est précis.')
+  st.write("La 'Mean Absolute Error' ou 'MAE' est de : " + str(MAE_KNNsm), ', plus elle est basse plus le modèle est précis. Notre modèle a donc ici une précision correcte, ce paramètre d erreur est cohérent et confirme le score de précision. ')
+              
+              #conf que c'est bien une prez de l'efficacité et evaluation de notre modèle, dans ce cas présenter tout en allant à essentiel, ajouter un réso de neurone pour comparer efficacité, des graphiques intéractifs sur ROC, les classes diparates et de la dataviz, des clics pour passer à l'étape suivante et une conclusion et limite de la suite des travaux.
