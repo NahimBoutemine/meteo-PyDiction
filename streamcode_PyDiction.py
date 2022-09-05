8faf71163edabea09d1562bd69dc7150eea99a2c
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-09-05 22:47:59 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -42,7 +42,7 @@ if rad == "Introduction : Le projet et ses créateurs":
     st.markdown("C'est un travail autour de la météorologie et du Machine Learning (ML). ")
     diapo = Image.open('diapo.jpg')
     st.image(diapo, caption="Etapes depuis les données météo jusqu'à la prédiction ")
-    st.markdown("Les données météorologiques de pluie, ensoleillement, températures, pression, vent, humidité, sur plusieurs années, sur 49 stations réparties sur tout le territoire australien.")
+    st.markdown("Les données sont issues de https://www.kaggle.com/jsphyg/weather-dataset-rattle-package et sont des données météorologiques de pluie, ensoleillement, températures, pression, vent, humidité, sur plusieurs années, sur 49 stations réparties sur tout le territoire australien.")
     st.markdown("Le but a été de contruire un modèle de prédiction de la variable nommée 'RainTomorrow'. ")
     st.markdown("'RainTomorrow' représente la présence de pluie à jour J + 1 sur un point du territoire australien, elle vaut tout simplement 1 si la pluie est > 1mm, 0 sinon.")
     st.markdown("Cette application streamlit vise à montrer les étapes ayant permis de conclure sur une méthode optimale de préparation des données et sur un modèle au taux de prédiction satisfisant pour ce type de données." )
