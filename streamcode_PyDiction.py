daab9b28b58fe0cf00aa6e03187a18697215e424
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-09-08 19:54:10 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -127,8 +127,8 @@ elif rad == "Préparation des données - partie 1 : élimination des manquantes,
   ##pourcentage de valeurs en fonction de chaque type d'objets et de valeurs manquantes
   fig1 = plt.figure()
   sns.countplot(data = df.dtypes.value_counts())
-  #st.pyplot(fig1)
- 
+  st.pyplot(fig1)
+  st.markdown("Les variables catégorielles consistuent un bon tier du jeu de données. L'évaluation de leur pertinence et leur encodage devra etre envisagé.")
   
   #afficher la répartition des valeurs dans la cible:
   st.markdown("Affichons la répartition des valeurs dans les catégories de la variable cible:")
