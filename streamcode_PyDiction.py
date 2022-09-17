ed5cc78be8601d787d42f6bdc46d52db4e2a1cdd
NahimBoutemine <108571173+NahimBoutemine@users.noreply.github.com>
2022-09-17 11:32:12 +0200

    Update streamcode_PyDiction.py

--- streamcode_PyDiction.py
+++ streamcode_PyDiction.py
@@ -167,6 +167,8 @@ elif rad == "Exploration des données brutes":
   st.header("Exploration des données brutes : préparer la suite...")
   st.markdown("Avant la modélisation, une présélection de modèles à tester est classiquement faite en fonction de critères sur le jeu de données exploré, ainsi que des sources bibliographiques. Le traitement des données avant la modélisation peut se faire de différentes manières, soit obligatoirement : élimination ou remplacement des données manquantes et des doublons, encodage des catégorielles, éventuellement: normalisation, rééchantilonnage, réduction du nombre de variables. Afin de déterminer la méthode amenant à un jeu de qualité optimale et donc des performances optimales, une exploration thématique des données brutes est nécessaire.")
   
+  df_sm.head(10)
+  
   #Nombre de données, et source :
   st.subheader("Source des données et nombre pour aider la préselction des modèles de ML :")   
   st.markdown("Le nombre de données est un critère pour la préselection de modèles et le choix du remplacement des manquantes. Les données sont présentes sur 49 stations australiennes, sur plusieurs années, et comprennent les informations journalières de : ensoleillement, humidité, vitesse et sens du vent, quantité de nuages, températures minimales et maximales etc.")
